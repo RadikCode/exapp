df60eb0c5912acd164fbfc368b499f15e126c1a7
RadikCode <radikbadr@gmail.com>
2022-04-10 12:50:18 +0300

    exApp_start

---
+++ fl_app_ex/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExApp());
+}
+
+class ExApp extends StatelessWidget {
+  const ExApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
